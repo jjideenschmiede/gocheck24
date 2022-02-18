80ccd0890d9df4f451c1a1e4fec173f61e660d37
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-18 08:54:25 +0100

    Add status codes function

--- status_codes.go
+++ status_codes.go
@@ -10,3 +10,23 @@
 //********************************************************************************************************************//
 
 package gocheck24
+
+import (
+	"errors"
+	"strings"
+)
+
+// statusCodes is to get the status code & return an error
+func statusCodes(status string) error {
+
+	// Check each status codes
+	switch {
+	case strings.Contains(status, "401"):
+		return errors.New("access is not authorized")
+	case strings.Contains(status, "404"):
+		return errors.New("not found")
+	default:
+		return nil
+	}
+
+}
