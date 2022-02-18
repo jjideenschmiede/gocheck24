43c772bf8123f8ff87b518cf2c58a0c2c70a9e08
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-18 08:55:15 +0100

    Add acknowledge function

--- acknowledge.go
+++ acknowledge.go
@@ -1,1 +1,34 @@
 package gocheck24
+
+import (
+	"fmt"
+)
+
+// Acknowledge is to set the order acknowledge to successful
+func Acknowledge(documentNumber int, r Request) error {
+
+	// Config new request
+	c := Config{
+		Path:   fmt.Sprintf("/shop/document/%d/acknowledge", documentNumber),
+		Method: "PUT",
+	}
+
+	// Send new request
+	response, err := c.Send(r)
+	if err != nil {
+		return err
+	}
+
+	// Close request body
+	defer response.Body.Close()
+
+	// Check response status
+	err = statusCodes(response.Status)
+	if err != nil {
+		return err
+	}
+
+	// Return data
+	return nil
+
+}
